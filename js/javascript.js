8224de11e5fcecc3c6f8b70d454a1b584430212e
RS09s <66675303+RS09s@users.noreply.github.com>
2021-01-20 16:45:22 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+$('.update').click(function(){
+  score.sum1.value = (parseInt(score.score11.value) + parseInt(score.score12.value) + parseInt(score.score13.value) + parseInt(score.score14.value)
+  + parseInt(score.score15.value) + parseInt(score.score16.value) + parseInt(score.score17.value) + parseInt(score.score18.value)
+  + parseInt(score.score19.value) + parseInt(score.score110.value) + parseInt(score.score111.value) + parseInt(score.score112.value));
+
+  score.sum2.value = (parseInt(score.score21.value) + parseInt(score.score22.value) + parseInt(score.score23.value) + parseInt(score.score24.value)
+  + parseInt(score.score25.value) + parseInt(score.score26.value) + parseInt(score.score27.value) + parseInt(score.score28.value)
+  + parseInt(score.score29.value) + parseInt(score.score210.value) + parseInt(score.score211.value) + parseInt(score.score212.value));
+
+  score.sum3.value = (parseInt(score.score31.value) + parseInt(score.score32.value) + parseInt(score.score33.value) + parseInt(score.score34.value)
+  + parseInt(score.score35.value) + parseInt(score.score36.value) + parseInt(score.score37.value) + parseInt(score.score38.value)
+  + parseInt(score.score39.value) + parseInt(score.score310.value) + parseInt(score.score311.value) + parseInt(score.score312.value));
+
+  score.sum4.value = (parseInt(score.score41.value) + parseInt(score.score42.value) + parseInt(score.score43.value) + parseInt(score.score44.value)
+  + parseInt(score.score45.value) + parseInt(score.score46.value) + parseInt(score.score47.value) + parseInt(score.score48.value)
+  + parseInt(score.score49.value) + parseInt(score.score410.value) + parseInt(score.score411.value) + parseInt(score.score412.value));
+
+});
